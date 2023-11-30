2f380a7c7a38f44eb24241e2ef4a690bee0e632d
MohamedAmhal <mohammedamhal349@gmail.com>
2023-11-30 04:40:01 -0800

    updating

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-import add_0 as n
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, n.add(a, b)))
+if __name__ == '__main__':
+    import add_0 as n
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, n.add(a, b)))
