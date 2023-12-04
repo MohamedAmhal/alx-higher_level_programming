5eb21c4f48671e6a964452ddb8faa329c204a27c
MohamedAmhal <mohammedamhal349@gmail.com>
2023-12-04 15:17:38 -0800

    updating the lsat task

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <Python.h>
+/**
+ * print_python_list_info -  printing the basics
+ * @p: the list in python
+ */
+void print_python_list_info(PyObject *p)
+{
+	int e;
+
+	printf("[*] Size of the Python List = %lu\n", Py_SIZE(p));
+	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
+	for (e = 0; e < Py_SIZE(p); e++)
+		printf("Element %d: %s\n", e, Py_TYPE(PyList_GetItem(p, e))->tp_name);
+}
