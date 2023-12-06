384e7e637d4a25fafcd1e0a27760e7d3fad6c349
MohamedAmhal <mohammedamhal349@gmail.com>
2023-12-06 15:36:57 -0800

    updating

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    a_dictionary = dict(a_dictionary)
+    a = []
+    for i in a_dictionary.keys():
+        a.append(i)
+
+    a.sort()
+    for i in a:
+        print(i, ": ", a_dictionary[i])
