eb9a07a456361bf0a0583316e39e0a329e25035b
MohamedAmhal <mohammedamhal349@gmail.com>
2023-12-06 15:03:10 -0800

    style betty

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -3,5 +3,5 @@ def number_keys(a_dictionary):
     a_dictionary = dict(a_dictionary)
     sum = 0
     for i in a_dictionary.keys():
-            sum = sum + 1
+        sum = sum + 1
     return sum
