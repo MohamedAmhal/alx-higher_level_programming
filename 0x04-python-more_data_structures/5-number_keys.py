a5423bdcdf10906a1a9013fdda445ace4c47d216
MohamedAmhal <mohammedamhal349@gmail.com>
2023-12-06 14:53:33 -0800

    updating the task 5

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -3,6 +3,6 @@ def number_keys(a_dictionary):
     a_dictionary = dict(a_dictionary)
     sum = 0
     for i in a_dictionary.keys():
-            sum += 1
+            sum = sum + 1
 
     return sum
