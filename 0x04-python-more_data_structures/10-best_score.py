84028d799b665b519978ebb138bad08d4f79ca53
MohamedAmhal <mohammedamhal349@gmail.com>
2023-12-06 16:49:02 -0800

    updating

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,10 +5,8 @@ def best_score(a_dictionary):
     a = []
     for i in a_dictionary.values():
         a.append(i)
-
     maxi = max(a)
     for i in a_dictionary.keys():
         if a_dictionary[i] == maxi:
             return i
-
     return None
