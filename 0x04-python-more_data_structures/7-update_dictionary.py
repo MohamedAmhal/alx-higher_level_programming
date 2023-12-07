7bea7801da3bbb9d94134434dfcd607d55e361c3
MohamedAmhal <mohammedamhal349@gmail.com>
2023-12-06 16:19:37 -0800

    updating

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,10 +2,9 @@
 def update_dictionary(a_dictionary, key, value):
     u = 0
     for i in a_dictionary.keys():
-            if i == key:
-                a_dictionary[key] = value
-                u = 1
-
+        if i == key:
+            a_dictionary[key] = value
+            u = 1
     if u == 0:
         a_dictionary[key] = value
     return a_dictionary
