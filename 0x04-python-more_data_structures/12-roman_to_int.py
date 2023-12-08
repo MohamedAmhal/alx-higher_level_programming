68b24e4595980264adff33fb8a1802a2b3e324c8
MohamedAmhal <mohammedamhal349@gmail.com>
2023-12-07 16:18:35 -0800

    updating

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if roman_string is None or type(roman_string) != str:
+        return 0
     sum = 0
     dernierelem = 0
     b = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
