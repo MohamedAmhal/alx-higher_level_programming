04e4c874a83b3a70fc4b49a81c73d9e85c68dd96
MohamedAmhal <mohammedamhal349@gmail.com>
2023-12-07 16:14:22 -0800

    updating

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    sum = 0
+    dernierelem = 0
+    b = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    for i in roman_string[::-1]:
+        v = b[i]
+        if v < dernierelem:
+            sum = sum - v
+        else:
+            sum = sum + v
+        dernierelem = v
+    return sum
