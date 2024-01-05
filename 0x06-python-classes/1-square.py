5f2fa6a04aa038516103a8eeb91e23a53ad7f203
MohamedAmhal <mohammedamhal349@gmail.com>
2024-01-05 15:48:58 -0800

    updating

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+this is application that have the class square
+"""
+
+
+class Square:
+    """
+    this is the class square
+    attribute size: private attribute
+    """
+    def __init__(self, __size):
+        self.__size = __size
