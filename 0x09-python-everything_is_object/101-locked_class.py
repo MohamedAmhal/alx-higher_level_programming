4e4ae09552c022f08e53760bb4c096f40b90be41
MohamedAmhal <mohammedamhal349@gmail.com>
2024-01-05 11:49:34 -0800

    up

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""Defines a locked class."""
+
+
 class LockedClass:
+    """
+    lockkked class
+    one attributtes : 'first_name'
+    """
 
     __slots__ = ["first_name"]
