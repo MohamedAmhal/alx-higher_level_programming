a943e890d2232b59716d8eac2b9150169f008387
MohamedAmhal <mohammedamhal349@gmail.com>
2024-01-05 16:18:37 -0800

    updating

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+this is a function class
+"""
+
+
+class Square:
+    """
+    this is the class square that contient theprivate attribute size
+    """
+    def __init__(self, __size=0):
+        self.__size = __size
+        if type(self.__size) != int:
+            raise ValueError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+    def size(self):
+        return self.__size
+
+    def size(self, value):
+        self.__size = value
+
+    def area(self):
+        return (self.__size) ** 2
