b1f420b36fde9ac95b92b00b1ae7ad8bd263eb98
MohamedAmhal <mohammedamhal349@gmail.com>
2024-01-06 09:25:30 -0800

    updating

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+this file is about creating a second class in my life hhh
+"""
+
+
+class Rectangle:
+    """
+    this is the class rectangle that we will discuss all informations
+    recently !!!
+    """
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    def width(self):
+        return self.__width
+
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    def height(self):
+        return self.__height
+
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
