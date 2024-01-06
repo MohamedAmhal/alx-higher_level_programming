4eba1c3b2a692ed6efd1858d6ba28a14dc1751ea
MohamedAmhal <mohammedamhal349@gmail.com>
2024-01-06 09:10:30 -0800

    updatong

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+this file is about creating a second class in my life hhh
+"""
+
+
+class Rectangle:
+    """
+    this is the class rectangle that we will discuss all informations
+    recently !!!
+    """
+
+    pass
