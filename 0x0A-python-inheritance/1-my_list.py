7fd12295cc09e5adaaa59e667802fd22e2d08a38
MohamedAmhal <mohammedamhal349@gmail.com>
2024-01-12 12:39:30 -0800

    updating

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+this folder that contient the class
+list for creating
+"""
+
+
+class list(MyList):
+    """
+    the class mylidet
+    """
+
+    def print_sorted(self):
+        return sorted(self)
