d7add0bbb67c17665a835fd42cce31f03384e515
MohamedAmhal <mohammedamhal349@gmail.com>
2024-01-13 08:38:43 -0800

    updating

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+this is the documantation
+of this function
+"""
+
+
+class BaseGeometry:
+    """
+    this is an empty class
+    """
+
+    def area(self):
+        """
+        Raises
+        ------
+        Exception
+            DESCRIPTION.
+
+        Returns
+        -------
+        None.
+
+        """
+        raise Exception("area() is not implemented")
