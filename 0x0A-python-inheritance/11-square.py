43a2fd8e11ab97cb9b450f5edbff3f583902e949
MohamedAmhal <mohammedamhal349@gmail.com>
2024-01-13 12:10:15 -0800

    updating

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Defines a class Rectangle that inherits from BaseGeometry."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    this is the documentation of this class
+    arguments:
+        size : size of the rectangle
+        return:
+            nothing
+    """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """
+        the method area
+        """
+        return self.__size ** 2
+
+    def __str__(self):
+        st = "[" + str(self.__class__.__name__) + "] "
+        st += str(self.__size) + "/" + str(self.__size)
+        return st
