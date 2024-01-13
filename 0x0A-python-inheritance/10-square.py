bb182f463d966bf1792b7739d30a220cc2cfed7f
MohamedAmhal <mohammedamhal349@gmail.com>
2024-01-13 11:58:09 -0800

    up

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 """Defines a class Rectangle that inherits from BaseGeometry."""
-
-
 BaseGeometry = __import__('9-base_geometry').BaseGeometry
 
 
@@ -15,6 +13,7 @@ class Square(Rectangle):
     """
     def __init__(self, size):
         self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
 
     def area(self):
