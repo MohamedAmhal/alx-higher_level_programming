96aeacbb5ee1f958f840e64b0a53db9f860af157
MohammedAmhal <mohammedamhal349@gmail.com>
2024-02-14 07:44:51 -0800

    updating the 15 files

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,4 @@
 -- this command is to show how to group a colum with another column in the table.
 SELECT score, COUNT(*) AS number FROM second_table
-GROUP BY score;
+GROUP BY score
+ORDER BY number DESC;
