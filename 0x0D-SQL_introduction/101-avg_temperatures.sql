ea5b94839896a4a26f130d6b646c09a82044557e
MohammedAmhal <mohammedamhal349@gmail.com>
2024-02-14 08:06:45 -0800

    updating

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,4 +1,4 @@
 -- this command is use to calaculate the average of the rows.
-SELECT city, AVG(value) AS avg_temp FROM temperature
+SELECT city, AVG(value) AS avg_temp FROM temperatures
 GROUP BY city
 ORDER BY avg_temp DESC;
